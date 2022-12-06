d538286857a929c59133acf00a4da7244a384b55
bmcminn <labs@gbox.name>
2022-12-06 00:48:07 -0600

    add page templates for contact, library, and local-guide

---
+++ wp-content/themes/legacy-avenue/js/vue-helpers.js
@@ -0,0 +1,87 @@
+
+/**
+ * Queries an api.
+ *
+ * @param      {<type>}   route   The route
+ * @param      {<type>}   params  The parameters
+ * @return     {Promise}  { description_of_the_return_value }
+ */
+async function queryApi(route, params) {
+	params = Object.assign({ per_page: 6 }, params)
+
+	const url = new URL(window.location)
+
+	url.pathname = `/wp-json/wp/v2/${route}`
+	url.search = new URLSearchParams(params)
+
+	const res = await fetch(url.toString(), {
+		method: 'get',
+	})
+
+	const data = await res.json()
+
+	return data
+}
+
+
+/**
+ * { function_description }
+ *
+ * @param      {string}  str     The string
+ * @return     {string}  { description_of_the_return_value }
+ */
+function stripExcerpt(str) {
+	return str.replace(/<a[\s\S]+?>[\s\S]+?<\/a>/gi, '')
+}
+
+
+/**
+ * { function_description }
+ *
+ * @param      {<type>}             date                   The date
+ * @param      {(Function|string)}  [format='YYYY-MM-DD']  The format
+ * @return     {<type>}             { description_of_the_return_value }
+ */
+function formatDate(date, format='YYYY-MM-DD') {
+	return dayjs(date).format(format)
+}
+
+
+/**
+ * Gets the post image.
+ *
+ * @param      {<type>}   imageId  The image identifier
+ * @return     {Promise}  The post image.
+ */
+async function getPostImage(imageId) {
+	let img = await queryApi(`media/${imageId}`)
+	return img
+}
+
+
+/**
+ * { function_description }
+ *
+ * @param      {<type>}   post    The post
+ * @return     {Promise}  { description_of_the_return_value }
+ */
+async function normalizePost(post) {
+	// modulate the excerpt
+	post.excerpt = post.excerpt.rendered.replace(/<\/?p[\s\S]*?>|<a[\s\S]+?>[\s\S]+?<\/a>/gi, '')
+		.trim()
+		.substr(0,200) + '...'
+
+	// provide
+	if (!!post.featured_media) {
+		let image = await getPostImage(post.featured_media)
+		post.featured_media = true
+		post.image_url = image.source_url
+		post.image_alt = image.alt_text
+	} else {
+		post.featured_media = true
+		post.image_url = defaultHero
+		post.image_alt = 'temp image'
+	}
+
+	return post
+}
---
+++ wp-content/themes/legacy-avenue/templates/vue-recommended-posts.php
@@ -0,0 +1,67 @@
+<?php
+
+wp_enqueue_script('legacyavenue_template_dayjs', 'https://cdnjs.cloudflare.com/ajax/libs/dayjs/1.11.6/dayjs.min.js', [], null, true);
+wp_enqueue_script('legacyavenue_template_vue', 'https://cdn.jsdelivr.net/npm/vue@2/dist/vue.js', [], null, true);
+wp_enqueue_script('legacyavenue_template_helpers', get_theme_file_uri() . '/js/vue-helpers.js', [], null, true);
+wp_enqueue_script('legacyavenue_template_recommended', get_theme_file_uri() . '/js/vue-recommended-posts.js', [], null, true);
+// <script src="https://cdnjs.cloudflare.com/ajax/libs/vue/2.7.14/vue.min.js" integrity="sha512-BAMfk70VjqBkBIyo9UTRLl3TBJ3M0c6uyy2VMUrq370bWs7kchLNN9j1WiJQus9JAJVqcriIUX859JOm12LWtw==" crossorigin="anonymous" referrerpolicy="no-referrer"></script>
+
+?>
+
+<section id="recommended-container" class="container">
+
+	<h2>Jesus<br>
+	RECOMMENDS</h2>
+
+	<div class="">
+		<div v-if="isLoading">
+			Loading articles...
+		</div>
+
+		<ul v-if="posts.length > 0" class="library-items flex row">
+			<li v-for="post in posts"
+				:key="post.id"
+				class="library-item xs-12 md-6 lg-4"
+			>
+				<div v-if="!!post.featured_media" class="image">
+					<img decoding="async"
+						loading="lazy"
+						:src="post.image_url"
+						:alt="post.image_alt"
+						class="alignnone size-full wp-image-128"
+						width="300"
+						height="150"
+					/>
+				</div>
+
+				<div class="title">
+					<h2 v-html="post.title.rendered"></h2>
+				</div>
+
+				<div class="excerpt">
+					<p v-html="post.excerpt"></p>
+				</div>
+
+				<time class="date"
+					:datetime="post.date"
+				>{{ formatDate(post.date) }}</time>
+
+				<div class="link">
+					<a :href="post.link">Learn More</a>
+				</div>
+			</li>
+		</ul>
+		<p v-else
+			class="empty-posts"
+		>
+			There are no posts to display. Please check back later.
+		</p>
+	</div>
+</section>
+
+
+<script>
+	const postsTags = <?php echo json_encode(get_tags()); ?>
+
+	const defaultHero = '<?php echo get_theme_file_uri() . '/assets/default-hero.png'; ?>'
+</script>
